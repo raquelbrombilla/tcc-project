cec65e1352375d60356ef9ed0ab34ba2bd27f3fe
Raquel Brombilla <raquelbrombilla11@gmail.com>
2025-02-09 15:26:02 -0300

    FEAT: Ajustada linguagem laravel

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'SoloCalc'),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
+    'locale' => 'pt_BR',
 
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
